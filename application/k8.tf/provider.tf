6914a94e5f036df5546cf29b7a9b860a779fc5b7
shashankaitha <shashankaitha8@gmail.com>
2024-04-28 00:01:10 +0530

    Folder Structure

--- application/k8.tf/provider.tf
+++ application/k8.tf/provider.tf
@@ -1,14 +1,7 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 terraform {
   required_version = ">= 1.6.0" #Forcing which version of Terraform needs to be used
   required_providers {
-    aws = {
-      version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
-      source  = "hashicorp/aws"
-    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.11.0"
@@ -16,7 +9,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "terraform-state-verito"
-    key            = "infra_state.tfstate"
+    key            = "application_statefile.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-verito-dynamodb"
     encrypt        = true
---
+++ infra/variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.large"
+}
+
+variable "subnet_id" {
+  default = "subnet-0c1253b5d27eccc55"
+}
+
+variable "project_name" {
+  default = "tekyz"
+}
+
+variable "security_group_id" {
+  default = "sg-01bd1299c5e24b487"
+}
+
+variable "key_name" {
+  default = "Kelly_vpc"
+}
