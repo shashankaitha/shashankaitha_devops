3aa6073b60ede30eaf8b0fb08b47f29781294ece
shashankaitha <shashankaitha8@gmail.com>
2024-04-27 23:26:37 +0530

    Installed minikube

--- ec2.tf
+++ ec2.tf
@@ -9,13 +9,36 @@ resource "aws_instance" "tekyz_demo" {
   ami                         = data.aws_ami.tekyz_ami.id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
   associate_public_ip_address = true
-  vpc_security_group_ids      = var.security_group_id
+
+  root_block_device {
+    volume_size           = "20"
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+  # security_groups             = var.security_group_id
   tags = {
     Name              = "${var.project_name}-minikube"
     Terraform-Managed = "Yes"
     Env               = "dev"
-    Projectname       = local.project_id
+    Projectname       = "1234"
   }
+  user_data = <<-EOF
+  	#!/bin/bash
+    sudo apt update && apt upgradeterraform_1.8.2_linux_386.zip
+    curl -fsSL https://get.docker.com -o install-docker.sh
+    chmod 755 install-docker.sh
+    sudo apt install unzip
+    sh install-docker.sh
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    curl -LO https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_linux_386.zip -o terraform_1.8.2_linux_386.zip
+    unzip terraform_1.8.2_linux_386.zip
+    sudo apt update && sudo apt install terraform
+    sudo apt install git-all
+  EOF
 
 }
--- variable.tf
+++ variable.tf
@@ -7,7 +7,7 @@ variable "instance_type" {
 }
 
 variable "subnet_id" {
-  default = "subnet-0ecb0ca491b829fc9"
+  default = "subnet-0c1253b5d27eccc55"
 }
 
 variable "project_name" {
@@ -16,4 +16,8 @@ variable "project_name" {
 
 variable "security_group_id" {
   default = "sg-01bd1299c5e24b487"
+}
+
+variable "key_name" {
+  default = "Kelly_vpc"
 }
