65ea115f2cbb8355203d7721e7380d0d4995b907
shashankaitha <shashankaitha8@gmail.com>
2024-04-27 23:34:09 +0530

    k8s

--- provider.tf
+++ provider.tf
@@ -9,6 +9,10 @@ terraform {
       version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
       source  = "hashicorp/aws"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
   }
   backend "s3" {
     bucket         = "terraform-state-verito"
@@ -17,4 +21,9 @@ terraform {
     dynamodb_table = "terraform-state-verito-dynamodb"
     encrypt        = true
   }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
 }
